f24de5e21cf80cdab298a3726752da35512eafe0
Hugo PLASTRE <hugo.plastre@gmail.com>
2024-10-28 14:31:18 +0100

    recompression photos + load window

--- assets/bootstrap/bootstrap.bundle.js
+++ assets/bootstrap/bootstrap.bundle.js
@@ -5,6 +5,9 @@
  */
 const carouselElement = document.getElementById("carouselExampleIndicators");
 carouselElement.classList.remove("slide");
+window.addEventListener("load", () => {
+  carouselElement.classList.add("slide");
+});
 (function (global, factory) {
   typeof exports === "object" && typeof module !== "undefined"
     ? (module.exports = factory())
