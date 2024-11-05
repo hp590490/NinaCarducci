1bf38bc2ce067cb73685f9e16144b7630ccf9768
Hugo PLASTRE <hugo.plastre@gmail.com>
2024-11-06 00:01:04 +0100

    low qualité image1 carousel

--- assets/scripts.js
+++ assets/scripts.js
@@ -1,15 +1,20 @@
-$(document).ready(function() {
-    $('.gallery').mauGallery({
-        columns: {
-            xs: 1,
-            sm: 2,
-            md: 3,
-            lg: 3,
-            xl: 3
-        },
-        lightBox: true,
-        lightboxId: 'myAwesomeLightbox',
-        showTags: true,
-        tagsPosition: 'top'
-    });
+$(document).ready(function () {
+  $(".gallery").mauGallery({
+    columns: {
+      xs: 1,
+      sm: 2,
+      md: 3,
+      lg: 3,
+      xl: 3,
+    },
+    lightBox: true,
+    lightboxId: "myAwesomeLightbox",
+    showTags: true,
+    tagsPosition: "top",
+  });
+});
+document.addEventListener("DOMContentLoaded", function () {
+  const img = document.querySelector("img.lazyload");
+  img.src = img.getAttribute("data-src");
+  img.classList.remove("lazyload");
 });
